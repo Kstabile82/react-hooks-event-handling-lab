3a641a1345bdf71816767195c151e23b02d776f2
KarinaStabile <karina.stabile@gmail.com>
2021-11-27 21:58:05 -0500

    commit tree abb886c4976179e5f732b64efe5fea16ef721060

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    onBlur != onFocus; 
+return (
+    <div>
+    <button
+     onBlur= {() => console.log('Good!')}
+     onFocus ={() => console.log('Hey! Eyes on me!')}
+    >Eyes On Me</button>
+     </div>
+    
+)
+}
+export default EyesOnMe; 
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
-// Code Keypad Component Here
+import React from "react";
+
+function KeyPad() {    
+    return (
+            <input
+            type="password"
+            name="pw"
+            onChange={(event) => console.log('Entering password...')}
+            placeholder="Enter Password"
+            />
+    )
+}
+export default KeyPad; 
